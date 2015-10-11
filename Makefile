0e275d40ce74999404dfc19c6d22ef46f24a5884
Georgy Curnoff <geocurnoff@gmail.com>
2015-10-10 23:50:37 -0600

    basic youtube staff

--- Makefile
+++ Makefile
@@ -1,6 +1,16 @@
 
-CFLAGS=$(shell pkg-config --cflags gio-2.0)
-LIBS=$(shell pkg-config --libs gio-2.0)
+CFLAGS=$(shell pkg-config --cflags gio-2.0) \
+       $(shell pkg-config --cflags json-glib-1.0) \
+			 $(shell curl-config --cflags)
+LIBS=$(shell pkg-config --libs gio-2.0) \
+     $(shell pkg-config --libs json-glib-1.0) \
+		 $(shell curl-config --libs)
 
 all:
 	gcc ${CFLAGS} yt-ringer-main.c -o yt-ringer ${LIBS}
+
+objects: *.c *.h
+	gcc ${CFLAGS} maman-bar.c objects-test.c -o objects ${LIBS}
+
+preprocess:
+	gcc -E ${CFLAGS} yt-ringer-main.c > preprocessed.c
--- yt-ringer-main.c
+++ yt-ringer-main.c
@@ -1,17 +1,50 @@
 #include <gio/gio.h>
 #include <stdio.h>
+#include <json-glib/json-glib.h>
+#include <curl/curl.h>
+#include <stdio.h>
+
+#include "maman-bar.h"
+
+size_t write_data(void *buffer, size_t size, size_t nmemb, void *userp) {
+  printf("%s\n", buffer);
+}
 
 activate (GApplication *app,
           gpointer      user_data)
 {
+
+  CURL *curl = curl_easy_init();
+  if(curl) {
+    CURLcode res;
+    curl_easy_setopt(curl, CURLOPT_URL, "https://www.googleapis.com/youtube/v3/channels?part=contentDetails&forUsername=exvsk&key=AIzaSyC6OAlPAuIa2YDC_w3xRrDMIX_M1fGF41g");
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+    res = curl_easy_perform(curl);
+    curl_easy_cleanup(curl);
+  }
+
   // most app logic is here
+  /* keep a loop
+     check if new video is available from any of the channels
+     use icon from channel, video title for notification title,
+     spawn notification
+
+     read videos by date: remember date of last check, read videos until finding an old video
+
+
+
+  */
+
+
+
   GNotification *notification;
   GFile *file;
   GIcon *icon;
-
-  notification = g_notification_new ("A Notification");
-  g_notification_set_body (notification, "Something happened");
-  file = g_file_new_for_path ("/usr/lib64/shotwell/plugins/builtin/youtube.png");
+  while(1){
+  sleep(1);
+  notification = g_notification_new ("ExVSK");
+  g_notification_set_body (notification, "Halo 3 ODST - Coop avec Kiki #1 [HD]");
+  file = g_file_new_for_path ("/tmp/photo.jpg");
   icon = g_file_icon_new (file);
   g_notification_set_icon (notification, G_ICON (icon));
   g_object_unref (icon);
@@ -19,6 +52,7 @@ activate (GApplication *app,
 
   g_application_send_notification (app, "new-video", notification);
   g_object_unref (notification);
+  }
 }
 
 int
